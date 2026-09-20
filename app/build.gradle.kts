0de1d94e58058294fcdfe03a12eb5be7f71480f1
Prem raj <prem736raj@gmail.com>
2026-09-20 12:28:22 +0530

    fix(play): target Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.spinbottle.truthdare.games"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.spinbottle.truthdare.games"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
